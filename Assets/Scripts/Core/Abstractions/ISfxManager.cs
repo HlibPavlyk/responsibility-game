4a6bc78675120ea6ace04f2003eb74fbf5f69b34
OlhaMish <olgami1402@gmail.com>
2025-12-05 18:35:12 +0200

    feat: extend ISfxManager with SfxOptions and new PlayClip methods

--- Assets/Scripts/Core/Abstractions/ISfxManager.cs
+++ Assets/Scripts/Core/Abstractions/ISfxManager.cs
@@ -1,14 +1,22 @@
-using UnityEngine; 
+using UnityEngine;
+using UnityEngine.Audio; 
 
 namespace Core.Abstractions
 {
+    public struct SfxOptions
+    {
+        public float volumeScale;
+        public float pitch;
+        public float spatialBlend;
+        public AudioMixerGroup mixer;
+        public float maxDistance;
+    } 
     public interface ISfxManager
     {
         void Play(string id, float volumeScale = 1f);
         void PlayAt(string id, Vector3 position, float volumeScale = 1f);
+        void PlayClip(AudioClip clip, in SfxOptions options);
+        void PlayClipAt(AudioClip clip, Vector3 position, in SfxOptions options);
         void Cleanup();
     }
-} 
-
- 
-
+}
