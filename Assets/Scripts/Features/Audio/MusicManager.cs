dfbd5957a63f70e305aa8f39147e51a643dbecf3
OlhaMish <olgami1402@gmail.com>
2025-12-07 02:07:26 +0200

    refactor: move MusicManagerSettings to GameSettings

--- Assets/Scripts/Features/Audio/MusicManager.cs
+++ Assets/Scripts/Features/Audio/MusicManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Core.Abstractions;
 using Core.Events;
+using Systems.Game;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using VContainer;
